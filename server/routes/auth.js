7387d6bbe4f51183e5488977c6aa917355898bf3
harshavardhan1905 <podetiharshavardhan@gmail.com>
2025-11-17 21:53:14 +0530

    new

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,7 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
+
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 require("dotenv").config();
