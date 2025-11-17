7ada5ba9899f3b044d47f5a2212a03e9c5dc1e07
harshavardhan1905 <podetiharshavardhan@gmail.com>
2025-11-17 21:58:19 +0530

    byscript

--- server/routes/all.js
+++ server/routes/all.js
@@ -1,5 +1,7 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
+
 const User = require("../models/User");
 const router = express.Router();
 
